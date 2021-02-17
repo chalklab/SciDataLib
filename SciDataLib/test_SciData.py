d3bac7d5766ccd5a60e0babfdee69fe94f9656eb
stuchalk <schalk@unf.edu>
2021-02-17 06:29:36 -0500

    added pytest test document and initial tests

---
+++ SciDataLib/test_SciData.py
@@ -0,0 +1,14 @@
+"""pytest test class"""
+from SciDataLib.SciData import SciData
+
+sd = SciData('example')
+
+
+def test_discipline():
+    assert sd.discipline('chemistry') == 'chemistry'
+
+
+def test_subdiscipline():
+    assert sd.subdiscipline('physicalchemistry') == 'physicalchemistry'
+
+
